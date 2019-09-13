7fd9027026835823c5769766a910d9dd014393bc
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-09-13 15:47:58 -0700

    cors

--- server.js
+++ server.js
@@ -36,12 +36,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  next();
-  });
+    res.header("Access-Control-Allow-Origin", "https://karuta-react.herokuapp.com/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // SECTION Routes
 // Get Root
