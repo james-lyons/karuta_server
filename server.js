cf1d5d01f15ede73ad8a0d800e810c8f431c63d0
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-09-13 17:10:08 -0700

    header

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(function(req, res, next) {
 });
 
 const corsOptions = {
-    origin: ['https://karuta-react.herokuapp.com/'],
+    origin: true,
     credentials: true,
     optionsSuccessStatus: 200
 };
