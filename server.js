7c955a2d4f20a4afe2888e0a86db189b79cc1288
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-09-14 22:05:03 -0700

    slash

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(session({
 // });
 
 const corsOptions = {
-    origin: ['https://karuta-react.herokuapp.com/'],
+    origin: ['https://karuta-react.herokuapp.com'],
     methods: ["PUT", "GET","POST", "DELETE", "OPTIONS"],
     credentials: true,
     optionsSuccessStatus: 200
