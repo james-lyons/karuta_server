5d176141c024b619c99407dbe8d08a9973640fd8
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-09-14 23:41:43 -0700

    changes

--- server.js
+++ server.js
@@ -28,14 +28,14 @@ app.use(session({
 }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://karuta-react.herokuapp.com"); 
+    res.header("Access-Control-Allow-Origin", "https://karuta-react.herokuapp.com/"); 
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 const corsOptions = {
-    origin: ["https://karuta-react.herokuapp.com/"],
+    origin: ["https://karuta-react.herokuapp.com"],
     credentials: true,
     optionsSuccessStatus: 200
 };
