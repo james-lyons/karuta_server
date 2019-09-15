ee25bb8a8f4eeedce6c487c715d7721bf2e90745
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-09-14 22:39:30 -0700

    status

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.use(session({
 }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Origin", "https://karuta-react.herokuapp.com/"); 
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    // res.header('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
